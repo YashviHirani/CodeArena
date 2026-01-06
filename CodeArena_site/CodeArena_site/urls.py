f29fddec2ade2b14fb38364555831e816fc62103
NivaPatel10 <nivapatel102006@gmail.com>
2026-01-06 10:38:03 +0530

    Integrated Login,sign up and home page

--- CodeArena_site/CodeArena_site/urls.py
+++ CodeArena_site/CodeArena_site/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup, name='signup'),
+    path('',views.home,name='home'),
 ]
