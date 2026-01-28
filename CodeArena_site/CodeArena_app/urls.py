227f11cbbd173df52aa2feb99d0a0a257063208a
Yashvi Hirani <yashvihirani0408@gmail.com>
2026-01-28 14:01:23 +0530

    Fix URLs and views after merge

--- CodeArena_site/CodeArena_app/urls.py
+++ CodeArena_site/CodeArena_app/urls.py
@@ -15,11 +15,16 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('profile/', views.profile_view, name='profile'),
-    path('quiz/', views.quiz_view, name='quiz'),
     path('insideQuiz/', views.insideQuiz_view, name='insideQuiz'),
     path('DebuggingQuiz/', views.DebuggingQuiz_view, name='DebuggingQuiz'),
     path('leaderboard/', views.leaderboard_view, name='leaderboard'),
     path("update-skills/", views.update_skills, name="update_skills"),
     path("problem/<int:problem_id>/", views.problem_detail_view, name="problem_detail"),
     path("submit/<int:problem_id>/", views.submit_solution, name="submit_solution"),
+
+
+    path('quiz/', views.quiz_home, name='quiz'),
+    path('quiz/start/', views.start_quiz, name='start_quiz'),
+    path('quiz/<int:language_id>/', views.inside_quiz, name='insideQuiz'),
+    path('quiz/save-answer/', views.save_mcq_answer, name='save_mcq_answer'),     
 ]
